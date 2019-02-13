9959ac3e1246a4b4a936e0b84809e46549ca71b3
Krispol <krispol@users.noreply.github.com>
2019-02-13 14:31:31 +0200

    Tunnitöö

--- Praktikum2/Versioon1/Tegelane.java
+++ Praktikum2/Versioon1/Tegelane.java
@@ -1,11 +1,17 @@
 class Tegelane{
   int x, y;
   Tyyp tyyp;
+  Suund suund;
   public Tegelane(){
     x=0; y=0;
     tyyp=Tyyp.MANGIJA;
+    suund=Suund.PAREMALE;
   }
+  public void liigu(){
+    if(suund==Suund.PAREMALE){x++;}
+  }
+  public void muudaSuund(Suund uusSuund){this.suund=uusSuund;}
   public String toString(){
-    return tyyp+" kohal"+x+", "+y;
+    return tyyp+" kohal "+x+", "+y+" suunaga "+suund;
   }
 }
